18db86a288486b8e6ca10db883a220c4799f2d67
mohan <mohan.chanti4@gmail.com>
2019-07-20 16:16:58 +0530

    new changes

---
+++ src/main/java/org/com/app/mohankumar2019/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mohankumar2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mohankumar2019" );
+    }
+}
